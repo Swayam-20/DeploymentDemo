f5e3431297a6fcafaef74dd58f0db7dcf0ec323a
SWAYAMJAISWAL <jaiswalswayam.jaiswal@gmail.com>
2025-07-01 14:10:19 +0530

    first commit
    first

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')// yeh common js hai
+// import express from 'express' // use this if using ES6 modules
+
+const app = express() // create an instance of express app
+// app have multiple methods like get, post, put, delete etc. 
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
+// To run this app, use the command: npm start
